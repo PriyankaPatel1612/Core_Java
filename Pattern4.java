288f0dbd47b7a0dd26077b326286cc6e7543b9ac
Priyanka Patel <priya.priyanka1612@gmail.com>
2023-01-01 09:39:33 +0530

    star pattern to form flip right angle triangle

---
+++ Pattern4.java
@@ -0,0 +1,45 @@
+/*
+Pattern to print
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+class Pattern4
+{
+  public static void main(String args[])
+  {
+    int i,j;
+    for(i=1 ; i<=5 ; i++)
+    {
+       for(j=5 ; j>=i ; j--)
+       {
+          System.out.print("* ");
+        }
+       System.out.println();
+    }
+   }
+}
+
+/*Or, we can also do this
+
+class Pattern4
+{
+  public static void main(String args[])
+  {
+    int i,j;
+    for(i=5 ; i>=1 ; i--)
+    {
+      for(j=1 ; j<=i ; j++)
+      {
+        System.out.print("* ");
+       }
+       System.out.println();
+      }
+   }
+}
+
+*/
