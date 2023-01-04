ca9824acbe2f517e2dc798af5d3f7e68d29bf9a3
Priyanka Patel <priya.priyanka1612@gmail.com>
2023-01-04 23:04:24 +0530

    number pattern

---
+++ Pattern21.java
@@ -0,0 +1,23 @@
+/*Pattern
+   1
+   2 3
+   4 5 6
+   7 8 9 10 
+*/
+
+class Pattern21
+{
+  public static void main(String args[])
+  {
+    int i,j,count=1;
+    for(i=1 ; i<=4 ; i++)
+    {
+      for(j=1 ; j<=i ; j++)
+      {
+        System.out.print(count + " ");
+        count++;
+       }
+      System.out.println();
+     }
+  }
+}
