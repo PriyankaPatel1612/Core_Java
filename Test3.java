cb80de6f8b52f9cd207819748f546c660edc247d
Priyanka Patel <priya.priyanka1612@gmail.com>
2023-01-05 15:30:20 +0530

    printing elements of array using for each loop

---
+++ Test3.java
@@ -0,0 +1,13 @@
+//Program to print array elements using for each loop
+
+class Test3
+{
+  public static void main(String args[])
+  {
+    int a[] = {10,20,30,40,50};
+    for(int i : a)
+    {
+      System.out.print(i+ "\t");
+    }
+  }
+}
