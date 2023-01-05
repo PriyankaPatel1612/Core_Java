1e6c477721816e22a1073692e56fe9ad69c4450c
Priyanka Patel <priya.priyanka1612@gmail.com>
2023-01-05 14:31:32 +0530

    program to print elements of array

---
+++ Test1.java
@@ -0,0 +1,13 @@
+/*Program to print all the elements of given array pn separate line */
+
+class Test1
+{
+  public static void main(String args[])
+  {
+    int a[] = {10,20,30,40,50};
+    for(int i=0 ; i<a.length ; i++)
+    {
+      System.out.print(a[i] + "\t");
+    }
+  }
+}
