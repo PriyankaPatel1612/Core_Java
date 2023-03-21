eeffd33feb669aa2f0e00bc7b6e0c3461acceb7e
Priyanka Patel <priya.priyanka1612@gmail.com>
2023-03-22 00:15:30 +0530

    some changes on Test14.java

--- Test14.java
+++ Test14.java
@@ -1,18 +1,20 @@
+
 //var args(variable arguments)
 public class Test14 {
-    public static void main(String[] args) {
-        Test14 obj = new Test14();
-        obj.add(10,20,30,40,50);
-        obj.add(1,2,3,4,5);
+     public static void main(String[] args) {
+         Test14 obj = new Test14();
+          //we can values as an argument, ...a (ellipsis a) take arguments and then used as array elements but the difference is that after the summation of elements it releases memory.
+         obj.add(10,20,30,40,50);
+         obj.add(1,2,3,4,5);
+     }
+     
+     void add(int ...a)
+    {
+         int sum=0;
+         for(int i=0 ;  i<a.length ; i++)
+         {
+              sum=sum+a[i];
+         }
+         System.out.println(sum);
     }
-    
-    void add(int ...a)
-   {
-        int sum=0;
-        for(int i=0 ;  i<a.length ; i++)
-        {
-             sum=sum+a[i];
-        }
-        System.out.println(sum);
-   }
-}
+ }
